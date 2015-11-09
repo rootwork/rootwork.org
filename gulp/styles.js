d5dff855b401b7eb1bfd3790a452c1f6fccfe598
Ivan Boothe <ivan@rootwork.org>
2015-11-09 01:02:21 -0800

    Hugo static site generator + Gulp task runner = win

---
+++ gulp/styles.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var cssGlobbing = require('gulp-css-globbing');
+var notify = require("gulp-notify");
+
+gulp.task('styles', function() {
+  return console.log('Bower: ' + mainBowerFiles());
+  return gulp.src(mainBowerFiles())
+  return gulp.src('src/styles/*.scss')
+        .pipe(cssGlobbing({
+          extensions: ['.scss']
+        }))
+        .pipe(sass({
+          sourcemap: true,
+          sourceComments: 'normal',
+          onError: function(err) {
+            return notify().write(err);
+          }
+        }))
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(minifycss({advanced:false}))
+        .pipe(gulp.dest('staging/css'));
+});
