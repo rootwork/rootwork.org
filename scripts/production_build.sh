fac801386c115bc4cb0da75c6894f1b2ed22e31b
Ivan Boothe <ivan@rootwork.org>
2021-08-19 16:29:50 -0700

    Changing production build script to flag from user input

--- scripts/production_build.sh
+++ scripts/production_build.sh
@@ -2,28 +2,28 @@
 
 # Ansi color code variables
 blue="\e[0;94m"
-green="\e[0;92m"
 expand_bg="\e[K"
 blue_bg="\e[0;104m${expand_bg}"
 reset="\e[0m"
 
 here=$(pwd) # Home directory
+notice="Exporting static site for production"
 vars="--gc"
-minify=""
+
+while getopts "m" flag; do
+  case "$flag" in
+    m)
+      vars="$vars --minify"
+      notice="$notice with minification disabled"
+      ;;
+  esac
+done
+
+notice="$notice..."
 
 echo -e "\n${blue_bg}${reset}"
-echo -e "${blue}Exporting static site for production...${reset}"
+echo -e "${blue} ${notice} ${reset}"
 echo -e "${blue_bg}${reset}\n"
-echo -e "${green}Minify HTML? (return for yes)${reset}"
-read minify
-
-# By default, we want it minified. If user enters anything other than a
-# return, don't include it. If you came here wanting to fine-tune how the
-# minification in Hugo happens, see
-# https://gohugo.io/getting-started/configuration/#configure-minify
-if [ -z "$minify" ]; then
-  vars="$vars --minify"
-fi
 
 cd './hugo'
 rm -rf public
