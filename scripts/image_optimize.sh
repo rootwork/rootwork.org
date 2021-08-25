4f58ca6974aa9c7b4e0ebb19a1c2795ae2217a5f
Ivan Boothe <ivan@rootwork.org>
2021-08-24 17:59:18 -0700

    Simplifying image optimization script

--- scripts/image_optimize.sh
+++ scripts/image_optimize.sh
@@ -10,13 +10,19 @@ red="\e[0;91m"
 reset="\e[0m"
 
 echo -e "\n${green}Optimizing images in content...${reset}"
+echo -e "${blue}"
 
 # Optimize png, jpg
-# Inspired by https://victoria.dev/blog/how-to-quickly-batch-resize-compress-and-convert-images-with-a-bash-one-liner/
-echo -e "${blue}"
-find hugo/content/ -not -path "*/static/*" \( -name '*.png' -o -name '*.jpg' -o -name '*.jpeg' \) -print0 | xargs -0 -P8 -n2 mogrify -strip -thumbnail '1000>' -verbose
+# 'mogrify' is imagemagick
+for f in ./hugo/content/**/*.{jpg,jpeg,png,gif}; do
+  if [ -e "$f" ] ; then
+    mogrify -strip -thumbnail '1000>' "$f";
+    echo -e "${f}";
+  fi
+done
+
 echo -e "${reset}"
-echo -e "${green}                               ...done.${reset}"
+echo -e "${green}                                ...done.${reset}"
 
 echo -e "\n${green}Creating modern image formats...${reset}"
 echo -e "${blue}"
@@ -24,21 +30,27 @@ echo -e "${blue}"
 # Create webp from jpeg and png files; gif files are likely to be larger in webp.
 # https://developers.google.com/speed/webp/docs/cwebp
 for f in ./hugo/content/**/*.{jpg,jpeg,png}; do
+  webp="${f%.*}.webp"
   if [ -e "$f" ] ; then
-    cwebp -quiet "$f" -o "${f%.*}.webp";
-    echo -e "${f%.*}.webp" | sed 's/^\.\///g';
+    if [ ! -e "${webp}" ]; then # Create only if file doesn't exist.
+      cwebp -quiet "$f" -o "${webp}";
+      echo -e "${webp}";
+    fi
   fi
 done
 
 # Create avif from webp files, and any gif files.
 # https://github.com/lovell/avif-cli
 for f in ./hugo/content/**/*.{webp,gif}; do
+  avif="${f%.*}.avif"
   if [ -e "$f" ] ; then
-    dir="$(dirname "${f}")"
-    npx avif --input="$f" --output="$dir"
-    echo -e "${f%.*}.avif" | sed 's/^\.\///g';
+    if [ ! -e "${avif}" ]; then # Create only if file doesn't exist.
+      dir="$(dirname "${f}")"
+      npx avif --input="$f" --output="$dir"
+      echo -e "${avif}";
+    fi
   fi
 done
 
 echo -e "${reset}"
-echo -e "${green}                               ...done.${reset}\n"
+echo -e "${green}                                ...done.${reset}\n"
