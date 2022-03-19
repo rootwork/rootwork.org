ce04c30c3c94f22ec23742a34106b6d93cd4c927
Ivan Boothe <git@rootwork.org>
2022-03-18 19:37:54 -0700

    current weather - fixes #10
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- hugo/assets/js/custom.js
+++ hugo/assets/js/custom.js
@@ -1,2 +1,34 @@
 // Enable CSS classes based on whether JS loads
 document.documentElement.classList.remove('no-js')
+
+// Get current weather
+function getWeather() {
+  const json_url = 'https://wttr.in/Portland?u&format=j1'
+  let xhr = new XMLHttpRequest()
+  xhr.open('GET', json_url, false)
+  xhr.send(null)
+
+  let weatherJSON = JSON.parse(xhr.responseText)
+
+  let weatherDesc =
+    weatherJSON.current_condition[0].weatherDesc[0].value.toLowerCase()
+  let weatherTemp = weatherJSON.current_condition[0].temp_F
+  let weatherWindDir = weatherJSON.current_condition[0].winddir16Point
+  let weatherWindSpeed = weatherJSON.current_condition[0].windspeedMiles
+
+  let weather =
+    weatherDesc +
+    ' and ' +
+    weatherTemp +
+    '°F, wind ' +
+    weatherWindDir +
+    ' at ' +
+    weatherWindSpeed +
+    'mph'
+
+  return weather
+}
+
+// Embed weather
+let weatherEl = document.getElementById('current-weather')
+weatherEl.innerHTML = getWeather()
