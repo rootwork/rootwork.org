172f8b9a594e9bce53442d17f571004c38c028d8
rootwork <rootwork@users.noreply.github.com>
2022-06-09 22:22:51 +0000

    deploy: 32e4c9756b876f43282ca7766d37ed5f4e19acb9

--- en/js/bundle.30fa0f2e4b375dc0bcbcf88aff77294a9626233be90b0ad9431466fa755d62777a96c2f62205e7aa10b6fa077567cac16e066a053c50472b473c26d25ffa5651.js
+++ en/js/bundle.30fa0f2e4b375dc0bcbcf88aff77294a9626233be90b0ad9431466fa755d62777a96c2f62205e7aa10b6fa077567cac16e066a053c50472b473c26d25ffa5651.js
@@ -1074,3 +1074,52 @@ window.addEventListener(pageHasLoaded, fileClosure());
 ;
 // Enable CSS classes based on whether JS loads
 document.documentElement.classList.remove('no-js')
+
+// Overriding subheading permalinks in Hugo Clarity
+// cf. https://github.com/chipzoller/hugo-clarity/blob/f062754faf7b5e92fa9dfc249b370022cc72738a/assets/js/index.js#L105-L115
+function headingPermalink() {
+  const baseURL = 'https://rootwork.github.io/rootwork.org/'
+
+  let headingNodes = [],
+    results,
+    link,
+    icon,
+    current,
+    id,
+    tags = ['h2', 'h3', 'h4', 'h5', 'h6']
+
+  current = document.URL
+
+  tags.forEach(function (tag) {
+    const article = elem('.post_content')
+    if (article) {
+      results = article.getElementsByTagName(tag)
+      Array.prototype.push.apply(headingNodes, results)
+    }
+  })
+
+  headingNodes.forEach(function (node) {
+    // Remove HC-created links
+    node.removeChild(node.firstElementChild)
+
+    // Create new links
+    link = createEl('a')
+    link.className = 'link icon'
+
+    // Create child span
+    let span = createEl('span')
+    link.appendChild(span)
+    span.className = 'permalink'
+    span.textContent = 'Permalink'
+
+    // Append URL fragment
+    id = node.getAttribute('id')
+    if (id) {
+      link.href = `${current}#${id}`
+      node.appendChild(link)
+      pushClass(node, 'link_owner')
+    }
+  })
+}
+
+headingPermalink()
