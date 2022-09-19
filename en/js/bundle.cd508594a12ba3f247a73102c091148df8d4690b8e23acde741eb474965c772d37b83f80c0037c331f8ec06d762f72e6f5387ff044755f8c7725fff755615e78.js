139267e51005ec3cef2fcc50f1fcc64f3773420c
rootwork <rootwork@users.noreply.github.com>
2022-09-19 08:17:06 +0000

    deploy: 7d375051e79a8c9cea6698d15361a5e9ff67f470

--- en/js/bundle.cd508594a12ba3f247a73102c091148df8d4690b8e23acde741eb474965c772d37b83f80c0037c331f8ec06d762f72e6f5387ff044755f8c7725fff755615e78.js
+++ en/js/bundle.cd508594a12ba3f247a73102c091148df8d4690b8e23acde741eb474965c772d37b83f80c0037c331f8ec06d762f72e6f5387ff044755f8c7725fff755615e78.js
@@ -1135,7 +1135,7 @@ function showDeadLink() {
     let span = createEl('span')
     link.appendChild(span)
     span.className = 'permalink'
-    span.textContent = 'Permalink'
+    span.textContent = ' Permalink'
 
     // Append URL fragment
     id = node.getAttribute('id')
